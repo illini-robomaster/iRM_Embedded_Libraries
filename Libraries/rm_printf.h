d931f4b8d88ea815cc66560e14afe0ec8b7b482e
alvin <alvinsunyixiao@gmail.com>
2018-04-14 20:09:49 -0500

    add rm printf

---
+++ Libraries/rm_printf.h
@@ -0,0 +1,14 @@
+//
+// Created by Yixiao Sun on 20/12/2017.
+//
+
+#ifndef IRM2018_RM_PRINT_H
+#define IRM2018_RM_PRINT_H
+
+#include "mytype.h"
+
+static char printf_temp[512];
+
+#define RM_PRINTF(expr...) ({ sprintf(printf_temp, expr); HAL_UART_Transmit(&huart6, printf_temp, strlen(printf_temp), 2); })
+
+#endif //IRM2018_RM_PRINT_H
