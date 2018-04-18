eaae3a48bfe19a9456e0a0eb8d7b3dc663718ffc
Nickel Liang <zuodong2@illinois.edu>
2018-04-18 01:52:22 -0500

    FIFO data structure adapted to the source

---
+++ Third_Party_Libraries/data_fifo.c
@@ -0,0 +1,188 @@
+/**
+ * @author  RoboMaster
+ * @date    2017-06
+ * @file    data_fifo.c
+ * @brief   Genernal FIFO model interface for any data element type.
+ * @log     2018-04-18 nickelliang
+ */
+
+#include "data_fifo.h"
+
+fifo_s_t* fifo_s_create(uint32_t unit_cnt, osMutexId mutex) {
+    fifo_s_t *pfifo     = NULL;
+    uint8_t  *base_addr = NULL;
+    //! Check input parameters.
+    ASSERT(0 != unit_cnt);
+    //! Allocate Memory for pointer of new FIFO Control Block.
+    pfifo = (fifo_s_t*) malloc(sizeof(fifo_s_t));
+    if (NULL == pfifo) {
+        //! Allocate Failure, exit now.
+        return (NULL);
+    }
+    //! Allocate memory for FIFO.
+    base_addr = malloc(unit_cnt);
+    if (NULL == base_addr) {
+        //! Allocate Failure, exit now.
+        return (NULL);
+    }
+    //! Initialize fifo
+    fifo_s_init(pfifo, base_addr, unit_cnt, mutex);
+    return (pfifo);
+}
+
+void fifo_s_destory(fifo_s_t* pfifo) {
+    //! Check input parameters.
+    ASSERT(NULL != pfifo);
+    ASSERT(NULL != pfifo->start_addr);
+    //! free FIFO memory
+    free(pfifo->start_addr);
+    //! delete mutex
+    osMutexDelete(pfifo->mutex);
+    //! free FIFO Control Block memory.
+    free(pfifo);
+    return;
+}
+
+int32_t fifo_s_init(fifo_s_t* pfifo, void* base_addr, uint32_t unit_cnt, osMutexId mutex) {
+    //! Check input parameters.
+    ASSERT(NULL != pfifo);
+    ASSERT(NULL != base_addr);
+    ASSERT(0    != unit_cnt);
+    //! Initialize the mutex
+    pfifo->mutex = mutex;
+    if (mutex != NULL) {
+        //! Initialize FIFO Control Block.
+        pfifo->start_addr  = (uint8_t*) base_addr;
+        pfifo->end_addr    = (uint8_t*) base_addr + unit_cnt - 1;
+        pfifo->buf_size    = unit_cnt;
+        pfifo->free        = unit_cnt;
+        pfifo->used        = 0;
+        pfifo->read_index  = 0;
+        pfifo->write_index = 0;
+        return 0;
+    }
+    else {
+        return -1; // Mutex invalid
+    }
+}
+
+int32_t fifo_s_put(fifo_s_t* pfifo, uint8_t element) {
+    //! Check input parameters.
+    ASSERT(NULL != pfifo);
+    //! Check if FIFO is full
+    if (0 >= pfifo->free) {
+        //! Error, FIFO is full!
+        return -1;
+    }
+    //! Update FIFO state
+    MUTEX_WAIT();
+    pfifo->start_addr[pfifo->write_index++] = element;
+    pfifo->write_index %= pfifo->buf_size;
+    pfifo->free--;
+    pfifo->used++;
+    MUTEX_RELEASE();
+    return 0;
+}
+
+int32_t fifo_s_puts(fifo_s_t *pfifo, uint8_t *psource, uint32_t number) {
+    int puts_num = 0;
+    //! Check input parameters.
+    ASSERT(NULL != pfifo);
+    //! Check element source validity
+    if(psource == NULL)
+        return -1;
+    //! Update FIFO structure
+    MUTEX_WAIT();
+    //! If FIFO is full during copy, abort. But try our best to fit data into FIFO. That's why we didn't use memcpy
+    for (uint32_t i = 0; (i < number) && (pfifo->free > 0); i++) {
+        pfifo->start_addr[pfifo->write_index++] = psource[i];
+        pfifo->write_index %= pfifo->buf_size;
+        pfifo->free--;
+        pfifo->used++;
+        puts_num++;
+    }
+    MUTEX_RELEASE();
+    return puts_num;
+}
+
+uint8_t fifo_s_get(fifo_s_t* pfifo) {
+    uint8_t retval = 0;
+    //! Check input parameters.
+    ASSERT(NULL != pfifo);
+    //! Update FIFO structure
+    MUTEX_WAIT();
+    retval = pfifo->start_addr[pfifo->read_index++];
+    pfifo->read_index %= pfifo->buf_size;
+    pfifo->free++;
+    pfifo->used--;
+    MUTEX_RELEASE();
+    return retval;
+}
+
+uint16_t fifo_s_gets(fifo_s_t* pfifo, uint8_t* source, uint8_t len) {
+    uint8_t   retval = 0;
+    //! Check input parameters.
+    ASSERT(NULL != pfifo);
+    //! Update FIFO structure
+    MUTEX_WAIT();
+    for (int i = 0; (i < len) && (pfifo->used > 0); i++) {
+        source[i] = pfifo->start_addr[pfifo->read_index++];
+        pfifo->read_index %= pfifo->buf_size;
+        pfifo->free++;
+        pfifo->used--;
+        retval++;
+    }
+    MUTEX_RELEASE();
+    return retval;
+}
+
+uint8_t fifo_s_pre_read(fifo_s_t* pfifo, uint8_t offset) {
+    uint32_t index;
+    //! Check input parameters.
+    ASSERT(NULL != pfifo);
+    if(offset > pfifo->used) {
+        return 0x00;
+    }
+    else {
+        index = ((pfifo->read_index + offset) % pfifo->buf_size);
+        //! Move Read Pointer to right position
+        return pfifo->start_addr[index];
+    }
+}
+
+uint8_t fifo_is_empty(fifo_s_t* pfifo) {
+    //! Check input parameter.
+    ASSERT(NULL != pfifo);
+    return (0 == pfifo->used);
+}
+
+uint8_t fifo_is_full(fifo_s_t* pfifo) {
+    //! Check input parameter.
+    ASSERT(NULL != pfifo);
+    return (0 == pfifo->free);
+}
+
+uint32_t fifo_used_count(fifo_s_t* pfifo) {
+    //! Check input parameter.
+    ASSERT(NULL != pfifo);
+    return (pfifo->used);
+}
+
+uint32_t fifo_free_count(fifo_s_t* pfifo) {
+    //! Check input parameter.
+    ASSERT(NULL != pfifo);
+    return (pfifo->free);
+}
+
+uint8_t fifo_flush(fifo_s_t* pfifo) {
+    //! Check input parameters.
+    ASSERT(NULL != pfifo);
+    //! Initialize FIFO Control Block.
+    MUTEX_WAIT();
+    pfifo->free        = pfifo->buf_size;
+    pfifo->used        = 0;
+    pfifo->read_index  = 0;
+    pfifo->write_index = 0;
+    MUTEX_RELEASE();
+    return 0;
+}
---
+++ Third_Party_Libraries/data_fifo.h
@@ -0,0 +1,156 @@
+/**
+ * @author  RoboMaster
+ * @date    2017-06
+ * @file    data_fifo.h
+ * @brief   Genernal FIFO model interface for any data element type.
+ * @log     2018-04-18 nickelliang
+ * @note    This is a circular buffer implementation of FIFO(Queue)
+ */
+
+#ifndef _DATA_FIFO_H_
+#define _DATA_FIFO_H_
+
+#include "stm32f4xx_hal.h"
+#include "cmsis_os.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ASSERT(x) do {                          \
+    while(!(x));                                \
+} while(0)                                      \
+
+#define MUTEX_WAIT() do {                       \
+    osMutexWait(pfifo->mutex, osWaitForever);   \
+} while(0)                                      \
+
+#define MUTEX_RELEASE() do {                    \
+    osMutexRelease(pfifo->mutex);               \
+} while(0)                                      \
+
+//! FIFO Memory Model (Single Byte Mode)
+typedef struct {
+    uint8_t   *start_addr;      // Start Address
+    uint8_t   *end_addr;        // End Address
+    uint32_t  free;             // The capacity of FIFO
+    uint32_t  buf_size;         // Buffer size
+    uint32_t  used;             // The number of elements in FIFO
+    uint8_t   read_index;       // Read Index Pointer
+    uint8_t   write_index;      // Write Index Pointer
+    osMutexId mutex;
+} fifo_s_t;
+
+/**
+ * Create a FIFO
+ *
+ * @param  unit_cnt   # of bytes to allocate
+ * @param  mutex      A mutex previously initialized
+ * @return            A fifo_s_t if success, NULL if failed
+ */
+fifo_s_t* fifo_s_create(uint32_t unit_cnt, osMutexId mutex);
+
+/**
+ * Destroy a FIFO
+ *
+ * @param  pfifo      FIFO to be destroyed
+ */
+void fifo_s_destory(fifo_s_t* pfifo);
+
+/**
+ * Initialize FIFO structure
+ *
+ * @param  pfifo      FIFO to be init
+ * @param  base_addr  Base addr of FIFO
+ * @param  unit_cnt   # of bytes allocated
+ * @param  mutex      FIFO mutex
+ * @return            -1 if mutex is invalid, 0 if success
+ */
+int32_t fifo_s_init(fifo_s_t* pfifo, void* base_addr, uint32_t unit_cnt, osMutexId mutex);
+
+/**
+ * Put an element into FIFO
+ *
+ * @param  pfifo      Pointer of valid FIFO
+ * @param  element    Data element you want to put
+ * @return            0 if success, -1 if FIFO is full
+ */
+int32_t fifo_s_put(fifo_s_t* pfifo, uint8_t element);
+
+/**
+ * Put some elements into FIFO
+ *
+ * @param  pfifo      Pointer of valid FIFO
+ * @param  psource    Data elements you want to put
+ * @param  number     Number of data elements
+ * @return            -1 if something wrong, other num indicate how many elements successfully put into FIFO
+ */
+int32_t fifo_s_puts(fifo_s_t *pfifo, uint8_t *psource, uint32_t number);
+
+/**
+ * Get an element from FIFO
+ *
+ * @param  pfifo      Pointer of valid FIFO
+ * @return            Data element poped from FIFO
+ */
+uint8_t  fifo_s_get(fifo_s_t* pfifo);
+
+/**
+ * Get elements from FIFO
+ *
+ * @param  pfifo      Pointer of a valid FIFO
+ * @param  source     Where to store poped elements
+ * @param  len        How many elements to retrive
+ * @return            Actual number of elements retrived
+ */
+uint16_t fifo_s_gets(fifo_s_t* pfifo, uint8_t* source, uint8_t len);
+
+/**
+ * Peek an element value at specific location
+ *
+ * @param  pfifo      Pointer of a valid FIFO
+ * @param  offset     Offset from current pointer
+ * @return            Data at offset location
+ */
+uint8_t  fifo_s_pre_read(fifo_s_t* pfifo, uint8_t offset);
+
+/**
+ * Check if FIFO is empty
+ *
+ * @param  pfifo      Pointer of a valid FIFO
+ * @return            1 if empty, 0 otherwise
+ */
+uint8_t  fifo_is_empty(fifo_s_t* pfifo);
+
+/**
+ * Check if FIFO is full
+ *
+ * @param  pfifo      Pointer of a valid FIFO
+ * @return            1 if full, 0 otherwise
+ */
+uint8_t  fifo_is_full(fifo_s_t* pfifo);
+
+/**
+ * Check how many entries are filled
+ *
+ * @param  pfifo      Pointer of a valid FIFO
+ * @return            # of used location
+ */
+uint32_t fifo_used_count(fifo_s_t* pfifo);
+
+/**
+ * Check how many entries are free
+ *
+ * @param  pfifo      Pointer of a valid FIFO
+ * @return            # of free location
+ */
+uint32_t fifo_free_count(fifo_s_t* pfifo);
+
+/**
+ * Flush the FIFO
+ *
+ * @param  pfifo      Pointer of a valid FIFO
+ * @return            0
+ * @note    Previous content still persist
+ */
+uint8_t fifo_flush(fifo_s_t* pfifo);
+
+#endif
