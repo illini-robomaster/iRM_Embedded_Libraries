be454c5de191d075cfd701375da9e6cb3523562a
alvin <alvinsunyixiao@gmail.com>
2018-04-18 12:27:59 -0500

    pid under construction, old referrence moved to from_pid

--- Libraries/pid.c
+++ Libraries/pid.c
@@ -1,156 +1,80 @@
 #include "pid.h"
+#include "motor.h"
+#include "bsp_error_handler.h"
 
-/* explicitly declare extern variables */
-motor_pid_t pid_pit;
-motor_pid_t pid_yaw;
-motor_pid_t pid_spd[4];
-motor_pid_t pid_launcher[2];
-motor_pid_t pid_poke;
-motor_pid_t pid_rotation;
-
-void abs_limit(float *candidate, float ABS_MAX) {
-    if(*candidate > ABS_MAX){
-        *candidate = ABS_MAX;
-        return;
-    }
-    if(*candidate < -ABS_MAX){
-        *candidate = -ABS_MAX;
-    }
-    return;
+int16_t get_prev_n_err(pid_ctl_t *pid, uint8_t n) {
+    return pid->err[(pid->idx - n) % HISTORY_DATA_SIZE];
 }
 
-static void pid_reset(motor_pid_t *pid, float kp, float ki, float kd, float kv) {
-    pid->p = kp;
-    pid->i = ki;
-    pid->d = kd;
-    pid->v = kv;
+int16_t position_pid_calc(pid_ctl_t *pid) {
+    int16_t err_now     = get_prev_n_err(pid, 0);
+    int16_t err_last    = get_prev_n_err(pid, 1);
+
+    float pout = pid->kp * err_now;
+    float iout = (pid->integrator += pid->ki * err_now);
+    float dout = (err_now - err_last) / pid->dt;
+
+    /* TODO: should return the true output */
+    return pout + iout + dout;
 }
 
-int get_circular_buffer_ind(int cur_idx, int enum_flag){
-    return (cur_idx + HISTORY_DATA_SIZE - enum_flag) % HISTORY_DATA_SIZE;
+int16_t delta_pid_calc(pid_ctl_t *pid) {
+    int16_t err_now     = get_prev_n_err(pid, 0);
+    int16_t err_last    = get_prev_n_err(pid, 1);
+    int16_t err_llast   = get_prev_n_err(pid, 2);
+
+    /* TODO: should return the true output */
+    return 0;
 }
 
-int motor_val_translate(float _input){
-    return _input;
+void pid_set_param(pid_ctl_t *pid, float kp, float ki, float kd) {
+    pid->kp = kp;
+    pid->ki = ki;
+    pid->kd = kd;
 }
 
-int pid_calc(motor_pid_t *my_motor_pid, float desired_value){
-    my_motor_pid->buffer_idx = (++my_motor_pid->buffer_idx) % HISTORY_DATA_SIZE;
-    my_motor_pid->set[my_motor_pid->buffer_idx] = motor_val_translate(desired_value);
-    my_motor_pid->err[my_motor_pid->buffer_idx] = 1;//TODO
+void pid_init(pid_ctl_t *pid, pid_mode_t mode, motor_t *motor, 
+        float kp, float ki, float kd, 
+        float max_err, float deadband, float dt) {
+    pid->mode       = mode;
+    pid->motor      = motor;
+    pid->max_err    = max_err;
+    pid->deadband   = deadband;
+    pid->dt         = dt;
+    pid->idx        = 0;
+    pid->integrator = 0;
+}
 
-    int LAST_POS = get_circular_buffer_ind(my_motor_pid->buffer_idx, LAST);
-    int LLAST_POS = get_circular_buffer_ind(my_motor_pid->buffer_idx, LLAST);
+void pid_angle_ctl_angle(pid_ctl_t *pid, int16_t target_angle) {
+    /* set angle error into the circular buffer */
+    uint8_t idx = (++pid->idx) % HISTORY_DATA_SIZE;
+    pid->err[idx] = get_angle_err(pid->motor, target_angle);
+    /* calculate generic posstion pid */
+    pid->motor->out = position_pid_calc(pid);
+}
 
-    switch(my_motor_pid->pid_mode) {
-        // Basic position PID; Used for chassis motor, gimbal auto-shooting mode
-        case POSITION_PID:
+void pid_speed_ctl_speed(pid_ctl_t *pid, int16_t target_speed) {
+    /* set speed error into the circular buffer */
+    uint8_t idx = (++pid->idx) % HISTORY_DATA_SIZE;
+    pid->err[idx] = get_speed_err(pid->motor, target_speed);
+    /* calculate generic position pid */
+    pid->motor->out = position_pid_calc(pid);
+}
+
+uint8_t pid_calc(pid_ctl_t *pid, int16_t target) {
+    switch (pid->mode) {
         case GIMBAL_AUTO_SHOOT:
+        case GIMBAL_MAN_SHOOT:
         case GIMBAL_MOUSE_IMU_SHOOT:
-        case GIMBAL_MAN_SHOOT: //manual shoot
-        case CHASSISS_ROTATE: // use chassis motor input to calibrate motor speed
-            { //delete this bracket may cause compiler error
-                // modified
-                if (my_motor_pid->max_err != 0 && fabsf(my_motor_pid->err[NOW]) > my_motor_pid->max_err){
-                    return 0;
-                } // PID does not apply if max_err=0 or err is larger than max_err; Keep original PID output
-                if (my_motor_pid->deadband != 0 && fabsf(my_motor_pid->err[NOW]) < my_motor_pid->deadband){
-                    return 0;
-                } // PID does not apply if deadband=0 or err is smaller than deadband; Keep original PID output
-
-                if(my_motor_pid->pid_mode != CHASSISS_ROTATE){
-                    if (my_motor_pid->err[NOW] >= 8191/2)
-                        my_motor_pid->err[NOW] = my_motor_pid->err[NOW] - 8191;	//expected - measured
-                    else if (my_motor_pid->err[NOW] <= -8191/2)
-                        my_motor_pid->err[NOW] = my_motor_pid->err[NOW] + 8191;	//expected - measured
-                }
-
-                if(my_motor_pid->pid_mode == GIMBAL_MOUSE_IMU_SHOOT){
-                    //do nothing for now
-                }
-
-                my_motor_pid->pout =  my_motor_pid->p *  my_motor_pid->err[NOW]; //pout=kp*err
-                my_motor_pid->iout += my_motor_pid->i *  my_motor_pid->err[NOW]; //iout=sum[ki*err]
-                my_motor_pid->dout =  my_motor_pid->d * (my_motor_pid->err[NOW] - my_motor_pid->err[LAST_POS]); //dout=kd*d(err)/dt
-                my_motor_pid->vout =  my_motor_pid->v * get_motor_d_angle(my_motor_pid->motor); //vout=kv*v_get
-
-                abs_limit(&(my_motor_pid->iout), my_motor_pid->IntegralLimit);
-                my_motor_pid->pos_out = my_motor_pid->pout
-                                        + my_motor_pid->iout + my_motor_pid->dout - my_motor_pid->vout;
-
-                abs_limit(&(my_motor_pid->pos_out), my_motor_pid->MaxOutput);
-                my_motor_pid->last_pos_out = my_motor_pid->pos_out; //Update last time
-                break;
-            }
-        case DELTA_PID:
-            if (my_motor_pid->err[NOW] >= 8191/2)
-                my_motor_pid->err[NOW] = my_motor_pid->err[NOW] - 8191;	//expected - measured
-            else if (my_motor_pid->err[NOW] <= -8191/2)
-                my_motor_pid->err[NOW] = my_motor_pid->err[NOW] + 8191;	//expected - measured
-
-            my_motor_pid->pout = my_motor_pid->p
-                            * (my_motor_pid->err[NOW] - my_motor_pid->err[LAST_POS]); //pout=kp*d(err)/dt
-            my_motor_pid->iout = my_motor_pid->i * my_motor_pid->err[NOW]; //iout=ki*err
-            my_motor_pid->dout = my_motor_pid->d
-                            * (my_motor_pid->err[NOW] - 2*my_motor_pid->err[LAST_POS] + my_motor_pid->err[LLAST_POS]); //dout=kd*d(d(err)/dt)/dt
-            //my_motor_pid->vout = my_motor_pid->v
-            //                * (my_motor_pid->get[NOW] - 2*my_motor_pid->get[LAST_POS] + my_motor_pid->get[LLAST_POS]); //vout=kv*a_get
-            my_motor_pid->vout = 0;
-
-            abs_limit(&(my_motor_pid->iout), my_motor_pid->IntegralLimit);
-
-            my_motor_pid->delta_u = my_motor_pid->pout
-                            + my_motor_pid->iout + my_motor_pid->dout - my_motor_pid->vout; //delta_u=pout+iout+dout-vout
-            my_motor_pid->delta_out = my_motor_pid->last_delta_out
-                            + my_motor_pid->delta_u; //delta_out=last_delta_out+delta_u
-            abs_limit(&(my_motor_pid->delta_out), my_motor_pid->MaxOutput);
-            my_motor_pid->last_delta_out = my_motor_pid->delta_out;
-
-            // modified
-            if (my_motor_pid->max_err != 0 && fabsf(my_motor_pid->err[NOW]) > my_motor_pid->max_err){
-                my_motor_pid->delta_out = 0;
-            } // PID does not apply if max_err=0 or err is larger than max_err; Keep original PID output
-            if (my_motor_pid->deadband != 0 && fabsf(my_motor_pid->err[NOW]) < my_motor_pid->deadband){
-                my_motor_pid->delta_out = 0;
-            } // PID does not apply if deadband=0 or err is smaller than deadband; Keep original PID output
-            break;
         case POKE:
-            my_motor_pid->pout = my_motor_pid->p * my_motor_pid->err[NOW]; //pout=kp*err
-            my_motor_pid->iout += my_motor_pid->i * my_motor_pid->err[NOW]; //iout=sum[ki*err]
-            my_motor_pid->dout = my_motor_pid->d * (my_motor_pid->err[NOW] - my_motor_pid->err[LAST_POS]); //dout=kd*d(err)/dt
-            my_motor_pid->vout = my_motor_pid->v * get_motor_d_angle(my_motor_pid->motor); //vout=kv*v_get
-            abs_limit(&(my_motor_pid->iout), my_motor_pid->IntegralLimit);
-            my_motor_pid->pos_out = my_motor_pid->pout + my_motor_pid->iout + my_motor_pid->dout - my_motor_pid->vout; //pos_out=pout+iout+dout-vout
-            abs_limit(&(my_motor_pid->pos_out), my_motor_pid->MaxOutput);
-            my_motor_pid->last_pos_out = my_motor_pid->pos_out; //Update last time
-            break;
+            pid_angle_ctl_angle(pid, target);
+            return 1;
+        case CHASSISS_ROTATE:
+            pid_speed_ctl_speed(pid, target);
+            return 1;
+        default:
+            bsp_error_handler(__FILE__, __LINE__, "pid mode does not exist");
+            return 0;
     }
-    return 1;
 }
 
-void PID_struct_init(motor_pid_t *pid, uint32_t mode, uint32_t maxout,
-        uint32_t intergral_limit, float _p, float _i, float _d, float _v) {
-    /*Initialize pid param */
-    pid->IntegralLimit = intergral_limit;
-    pid->MaxOutput = maxout;
-    pid->pid_mode = mode;
-
-    pid->set = (uint16_t *)malloc(HISTORY_DATA_SIZE * sizeof(uint16_t));
-    pid->err = (uint16_t *)malloc(HISTORY_DATA_SIZE * sizeof(uint16_t));
-    //initialize the dynamic array to prevent garbage value
-    for(int i = 0; i < HISTORY_DATA_SIZE; i++){
-        pid->set[i] = 0;
-        pid->err[i] = 0;
-    }
-
-    pid->buffer_idx = 0;
-
-    pid->p = _p;
-    pid->i = _i;
-    pid->d = _d;
-    pid->v = _v;
-
-    /*set the default value for deadband and max_err*/
-    pid->deadband = 1.0f; // set PID output tolerance
-    pid->max_err = maxout; // set error upper bound
-}
--- Libraries/pid.h
+++ Libraries/pid.h
@@ -15,103 +15,32 @@ void abs_limit(float *candidate, float ABS_MAX);
 
 #define HISTORY_DATA_SIZE 4
 
-enum{
-    NOW    = 0,
-    LAST   = 1,
-    LLAST  = 2,
-    LLLAST = 3,
-};
-
-enum{
-    POSITION_PID,
-    DELTA_PID,
+typedef enum {
     GIMBAL_AUTO_SHOOT,
     GIMBAL_MAN_SHOOT,
     GIMBAL_MOUSE_IMU_SHOOT,
     POKE,
     CHASSISS_ROTATE
-};
+}   pid_mode_t;
 
-typedef struct __motor_pid_t
+typedef struct pid_ctl_s
 {
-    float p;
-    float i;
-    float d;
-    float v;
+    float   kp;
+    float   ki;
+    float   kd;
+    float   integrator;
+    float   max_err;
+    float   deadband;
+    float   dt;
 
-    motor_t *motor; //motor it controls
+    pid_mode_t  mode;
+    motor_t     *motor;
 
-    /* arrays that contain recent data (4 recent calls) */
-    uint16_t *set; //target value
-    uint16_t *err; //error
-    int buffer_idx;
+    int16_t err[HISTORY_DATA_SIZE];
+    uint8_t idx;
 
-    float pout; //calculated p output
-    float iout; //calculated i output
-    float dout; //calculated d output
-    float vout; //calculated v output
-
-    float pos_out; //new (current) position output (to be read by other program)
-    float last_pos_out; //last position output
-    float delta_u; //derivative of delta_out
-    float delta_out; //new (current) delta output (to be read by other program) = last_delta_out + delta_u
-    float last_delta_out; //last delta out
-
-    float max_err;  //error threshold
-    float deadband; //accuracy threshold. see https://en.wikipedia.org/wiki/PID_controller#Deadband
-    uint32_t pid_mode; //for switch cases
-    uint32_t MaxOutput; //output threshold; should be customized for each motor
-    uint32_t IntegralLimit; //Integral limit
-} motor_pid_t;
-
-/**
- * helper function to init a motor_pid_t
- * @brief
- * @param pid             pid obj to be initialized (i.e., which motor to be configured)
- * @param mode            pid mode for this motor_pid
- *                              for explanation of mode, refer to header
- * @param maxout          Maximum output of this pid
- * @param intergral_limit Intergral limit
- * @param _p              user p
- * @param _i              user i
- * @param _d              user d
- * @param _v              user v
- */
-void PID_struct_init( motor_pid_t* pid, uint32_t mode,  uint32_t maxout,
-    uint32_t intergral_limit, float kp, float ki, float kd, float kv);
-
-/**
- * @brief reset p/i/d/v config of desired motor pid
- * @param pid pointer to desired motor pid
- * @param kp  new p value
- * @param ki  new i value
- * @param kd  new d value
- * @param kv  new v value
- */
-static void pid_reset(motor_pid_t *pid, float kp, float ki, float kd, float kv);
-
-/**
- * @brief  translate user-friendly input value into motor raw value
- *              (do nothing for now; edit it after finishing pid.)
- * @param  _input user input value e.g., desired angle
- * @return        corresponding raw value
- */
-int motor_val_translate(float _input);
-
-/**
- * @brief  calculate pid output given pid struct,
- *                              observed value and desired value. Designed for 3508 and 6623 motor
- * @param  my_motor desired pid struct
- * @param  desired_value target value
- * @return 1 or 0, where 0 means it doesn't modify anything (because constraints are not satisfied)
- */
-int pid_calc(motor_pid_t *my_motor_pid, float desired_value);
-
-extern motor_pid_t pid_pit;
-extern motor_pid_t pid_yaw;
-extern motor_pid_t pid_spd[4];
-extern motor_pid_t pid_launcher[2];
-extern motor_pid_t pid_poke;
-extern motor_pid_t pid_rotation;
+    uint32_t MaxOutput;
+    uint32_t IntegralLimit;
+}   pid_ctl_t;
 
 #endif
