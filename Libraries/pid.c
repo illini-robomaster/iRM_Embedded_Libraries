f3129e19db7f322d54746cb17221b2b0ebe68aa6
Roger Q <twofs0.5@gmail.com>
2018-04-18 17:32:25 -0500

    deprecate overflow

--- Libraries/pid.c
+++ Libraries/pid.c
@@ -3,11 +3,11 @@
 #include "bsp_error_handler.h"
 
 int16_t get_prev_n_err(pid_ctl_t *pid, uint8_t n) {
-    return pid->err[(pid->idx - n) % HISTORY_DATA_SIZE];
+    return pid->err[(pid->idx + HISTORY_DATA_SIZE - n) % HISTORY_DATA_SIZE];
 }
 
 int16_t position_pid_calc(pid_ctl_t *pid) {
-    int16_t err_now     = get_prev_n_err(pid, 0);
+    int16_t err_now     = pid->err[pid->idx];
     int16_t err_last    = get_prev_n_err(pid, 1);
 
     float pout = pid->kp * err_now;
@@ -32,7 +32,7 @@ void pid_set_param(pid_ctl_t *pid, float kp, float ki, float kd) {
     pid->kd = kd;
 }
 
-void pid_init(pid_ctl_t *pid, pid_mode_t mode, motor_t *motor, 
+void pid_init(pid_ctl_t *pid, pid_mode_t mode, motor_t *motor,
         float kp, float ki, float kd,
         float low_lim, float high_lim, float deadband, float dt) {
     pid->mode       = mode;
@@ -52,8 +52,8 @@ void pid_angle_ctl_angle(pid_ctl_t *pid, int16_t target_angle) {
     else if (clip_angle_err(pid->motor, target_angle - pid->high_lim) > 0)
         target_angle = pid->high_lim;
     /* set angle error into the circular buffer */
-    uint8_t idx = (++pid->idx) % HISTORY_DATA_SIZE;
-    pid->err[idx] = get_angle_err(pid->motor, target_angle);
+    pid->idx = (++pid->idx) % HISTORY_DATA_SIZE;
+    pid->err[pid->idx] = get_angle_err(pid->motor, target_angle);
     /* calculate generic posstion pid */
     pid->motor->out = position_pid_calc(pid);
 }
@@ -65,8 +65,8 @@ void pid_speed_ctl_speed(pid_ctl_t *pid, int16_t target_speed) {
     else if (target_speed - pid->high_lim > 0)
         target_speed = pid->high_lim;
     /* set speed error into the circular buffer */
-    uint8_t idx = (++pid->idx) % HISTORY_DATA_SIZE;
-    pid->err[idx] = get_speed_err(pid->motor, target_speed);
+    pid->idx = (++pid->idx) % HISTORY_DATA_SIZE;
+    pid->err[pid->idx] = get_speed_err(pid->motor, target_speed);
     /* calculate generic position pid */
     pid->motor->out = position_pid_calc(pid);
 }
@@ -87,4 +87,3 @@ uint8_t pid_calc(pid_ctl_t *pid, int16_t target) {
             return 0;
     }
 }
-
