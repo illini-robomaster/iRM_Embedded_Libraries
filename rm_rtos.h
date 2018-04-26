dfb4ce33e3722e692f2c31f92c87ea89c35a352d
Roger Q <twofs0.5@gmail.com>
2018-04-26 01:27:41 -0500

    RTOS looks good for now

--- rm_rtos.h
+++ rm_rtos.h
@@ -76,8 +76,8 @@ extern inline void RM_RTOS_TIMERS_Init(void) {
  * @date    2018-04-13
  */
 extern inline void RM_RTOS_THREADS_Init(void) {
-    //referee_task_create();
-    //imu_task_create();
+    referee_task_create();
+    imu_task_create();
     //gimbal_task_create();
     chassis_task_create();
 }
@@ -117,7 +117,7 @@ extern inline void RM_RTOS_Default_Task(void const * argument) {
  * @author Nickel_Liang
  * @date   2018-04-16
  */
-extern inline void RM_RTOSRM_Main_Init_Ready() {
+extern inline void RM_RTOS_Ready() {
     /* Indicate RTOS booted. Ready to battle. */
     buzzer_sing_song(startup, 1);
     led_green_on();
