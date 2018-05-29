8696592ac0e78689385d45645fc43fc4ca026b54
alvin <alvinsunyixiao@gmail.com>
2018-05-29 18:52:26 +0800

    adc under dma mode fully tested and functional

---
+++ BSP/bsp_adc.h
@@ -0,0 +1,52 @@
+#ifndef _BSP_ADC_H_
+#define _BSP_ADC_H_
+
+#include "stm32f4xx_hal_adc.h"
+#include "adc.h"
+#include "bsp_error_handler.h"
+
+#define BSP_ADC1_CHANNEL_SIZE 2
+// #define BSP_ADC2_CHANNEL_SIZE 1
+// #define BSP_ADC3_CHANNEL_SIZE 1
+
+#define MAX_VOLT    3.3
+#define MAX_ADC_OUT 4095
+
+/**
+ * @brief enable generic ADC controller under DMA mode
+ * @param hadc      ADC handle
+ * @param pData     address of the first converted value
+ * @param channel   total channel size for ADC1 (same as the maximum rank number marked
+ *                  in ioc setting)
+ * @return  1 for success, 0 for failure 
+ */
+uint8_t adc_dma_enable(ADC_HandleTypeDef *hadc, uint16_t *pData, uint32_t channel);
+
+/**
+ * @brief enable ADC1 controller under DMA mode
+ * @return 1 for suecess, 0 fo r failure
+ */
+uint8_t adc1_dma_enable(void);
+
+/**
+ * @brief read raw value from ADC1
+ * @param channel which channel to be read (same as the rank number marked in ioc settings)
+ * @return analog output ranging from 0 ~ 4095 (a read failure will return 0) 
+ */
+uint8_t adc1_get_val(uint8_t channel);
+
+/**
+ * @brief read voltage value from ADC1
+ * @param channel which channel to be read (same as the rank number marked in ioc settings)
+ * @return voltage value ranging from 0 ~ 3.3 (a read failure will return 0)
+ */
+float adc1_get_volt(uint8_t channel);
+
+/**
+ * @brief convert raw analog output to voltage value
+ * @param raw_data raw analog output ranging from 0 ~ 4096
+ * @return  converted voltage value 
+ */
+float get_volt_from_raw_data(uint16_t raw_data);
+
+#endif
---
+++ Tests/test_bsp_adc.c
@@ -0,0 +1,14 @@
+#include "test_bsp_adc.h"
+#include "cmsis_os.h"
+
+uint16_t data[2];
+
+void test_bsp_adc(void) {
+    adc1_dma_enable();
+    while (1) {
+        uint16_t chan0 = adc1_get_val(0);
+        uint16_t chan1 = adc1_get_val(1);
+        print("channel 0: %d channel 1: %d\r\n", chan0, chan1);
+        osDelay(300);
+    }
+}
