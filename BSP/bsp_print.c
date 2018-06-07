84fe6021e414b5e2e19574983cd11d14a321712f
alvin <alvinsunyixiao@gmail.com>
2018-06-07 22:55:48 +0800

    change print buffer from static to dynamic to avoid thread conflict

--- BSP/bsp_print.c
+++ BSP/bsp_print.c
@@ -3,15 +3,15 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
-char printf_temp[256];
-
 void print(const char *fmt, ...) {
     if (PRINT_TO_UART == 1) {
+        char *printf_temp = pvPortMalloc(512*sizeof(char));
         va_list args;
         va_start(args, fmt);
         vsprintf(printf_temp, fmt, args);
         va_end(args);
         HAL_UART_Transmit(&BSP_PRINT_PORT, (uint8_t*)printf_temp, strlen(printf_temp), 200);
+        vPortFree(printf_temp);
     }
     if (PRINT_TO_SD == 1)
         return;
