be591195c019fb756104ad0a4a339918fbc12b67
alvin <alvinsunyixiao@gmail.com>
2018-06-19 16:42:16 +0800

    pwm test changed to two motors

--- Tests/test_bsp_pwm.c
+++ Tests/test_bsp_pwm.c
@@ -2,15 +2,17 @@
 #include "cmsis_os.h"
 
 void test_bsp_pwm() {
-    pwm_t snail_2308;
-    pwm_init(&snail_2308, &htim12, 2);
+    pwm_t snail_left, snail_right;
+    pwm_init(&snail_left, &htim12, 2);
+    pwm_init(&snail_right, &htim12, 1);
 
     osDelay(4000);
     
     while (1) {
-        for (size_t i = 1100; i < 1230; i += 10) {
-            pwm_set_pulse_width(&snail_2308, i);
-            osDelay(1000);
+        for (size_t i = 1100; i < 1300; i += 50) {
+            pwm_set_pulse_width(&snail_left, i);
+            pwm_set_pulse_width(&snail_right, i);
+            osDelay(10000);
         }
     }
 }
