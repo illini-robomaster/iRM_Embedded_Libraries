de0d3a8131e8394b5d447cbe77b04bb374b6cccb
alvin <alvinsunyixiao@gmail.com>
2018-07-09 23:58:48 +0800

    change tof test to adapt to multiple sensors

--- Tests/test_bsp_tof.c
+++ Tests/test_bsp_tof.c
@@ -4,11 +4,13 @@
 
 void test_bsp_tof(void) {
     bsp_vl53l0x_t *tof1 = bsp_vl53l0x_init(0x80, GPIOC, GPIO_PIN_0, 30000);
+    bsp_vl53l0x_t *tof2 = bsp_vl53l0x_init(0x70, GPIOC, GPIO_PIN_1, 30000);
     
-    uint16_t dist1;
+    uint16_t dist1, dist2;
     while (1) {
         dist1 = get_vl53l0x_dist_milli(tof1);
-        print("dist1: %d\r\n", dist1);
+        dist2 = get_vl53l0x_dist_milli(tof2);
+        print("dist1: %d dist2: %d\r\n", dist1, dist2);
         osDelay(50);
     }
 }
